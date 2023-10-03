b4a10029817ac0660d70cdf15d8af06ee01dce91
amaliatyler <amaliatyler@gmail.com>
2023-10-03 18:35:55 +0300

    рефакторинг кода с помощью createEntityAdapter в heroesList, heroesSlice

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -2,8 +2,7 @@ import { useHttp } from '../../hooks/http.hook';
 import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { createSelector } from '@reduxjs/toolkit';
-import { heroDeleted, fetchHeroes } from './heroesSlice';
+import { heroDeleted, fetchHeroes,filteredHeroesSelector } from './heroesSlice';
 import HeroesListItem from '../heroesListItem/HeroesListItem';
 import Spinner from '../spinner/Spinner';
 
@@ -13,17 +12,6 @@ import Spinner from '../spinner/Spinner';
 // Удаление идет и с json файла при помощи метода DELETE
 
 const HeroesList = () => {
-    const filteredHeroesSelector = createSelector(
-        (state) => state.filters.activeFilter,
-        (state) => state.heroes.heroes,
-        (filter, heroes) => {
-            if (filter === 'all') {
-                return heroes;
-            } else {
-                return heroes.filter((item) => item.element === filter);
-            }
-        }
-    );
 
     const filteredHeroes = useSelector(filteredHeroesSelector);
     const heroesLoadingStatus = useSelector(
@@ -74,7 +62,9 @@ const HeroesList = () => {
 
     const elements = renderHeroesList(filteredHeroes);
 
-    return <TransitionGroup component="ul">{elements}</TransitionGroup>;
+    return <TransitionGroup component="ul">
+                {elements}
+            </TransitionGroup>;
 };
 
 export default HeroesList;
--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -1,10 +1,15 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, createEntityAdapter, createSelector } from "@reduxjs/toolkit";
 import { useHttp } from "../../hooks/http.hook";
 
-const initialState = {
-    heroes: [],
+// при вызове эта функция вернет объект с готовыми методами, коллбэками, мемоизир ованными селекторами и определенной структурой
+// обычно адаптер передается в качестве начального значения при создании слайса
+// при создании адаптер может принять в себя объект с коллбэками 
+const heroesAdapter = createEntityAdapter();
+
+
+const initialState = heroesAdapter.getInitialState({
     heroesLoadingStatus: 'idle',
-};
+});
 
 export const fetchHeroes = createAsyncThunk(
     'heroes/fetchHeroes',
@@ -23,8 +28,8 @@ const heroesSlice = createSlice({
     name: 'heroes',
     initialState,
     reducers: {
-        heroCreated: (state, action) => { state.heroes.push(action.payload)},
-        heroDeleted: (state, action) => { state.heroes = state.heroes.filter(item => item.id !== action.payload)}
+        heroCreated: (state, action) => { heroesAdapter.addOne(state, action.payload)},
+        heroDeleted: (state, action) => { heroesAdapter.removeOne(state, action.payload)}
     },
     // т.к. fetchHeroes создается как бы вне createSlice, они считаются сторонними actionCreators
     // они записываются в поле для extraReducers
@@ -33,7 +38,9 @@ const heroesSlice = createSlice({
             .addCase(fetchHeroes.pending, state => { state.heroesLoadingStatus = 'loading'})
             .addCase(fetchHeroes.fulfilled, (state, action) => {
                 state.heroesLoadingStatus = 'idle';
-                state.heroes = action.payload;
+                // setAll полностью заменит изначальные данные на пришедшие
+                // для обычного добавления новых данных использовать setMany
+                heroesAdapter.setAll(state, action.payload);
             })
             .addCase(fetchHeroes.rejected, state => { state.heroesLoadingStatus = 'error'})
             .addCase(() => {})
@@ -44,6 +51,22 @@ const heroesSlice = createSlice({
 const { actions, reducer } = heroesSlice;
 
 export default reducer;
+
+// создаем объект, вытаскиваем сразу selectAll из этого объекта и экспортируем для использования в HeroesList
+const {selectAll} = heroesAdapter.getSelectors(state => state.heroes);
+// сразу экспотируем готовый селектор
+export const filteredHeroesSelector = createSelector(
+    (state) => state.filters.activeFilter,
+    selectAll,
+    (filter, heroes) => {
+        if (filter === 'all') {
+            return heroes;
+        } else {
+            return heroes.filter((item) => item.element === filter);
+        }
+    }
+);
+
 export const {
     heroesFetching, 
     heroesFetched,
